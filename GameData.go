a9566aae9090ccfb73382492e70be18172aaacf2
shinjitsu <shinjitsu@auxilioabalto.me>
2024-10-09 15:44:23 -0400

    Data Structures are probably complete for first pass at game. Begin work on run server - currently very incomplete.

--- GameData.go
+++ GameData.go
@@ -4,27 +4,80 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+type SpecialAbility int
+
+const (
+	None SpecialAbility = iota
+	Flying
+	Magic
+	ChargeBonus
+	RangedAttack
+)
+
+type LandType int
+
+const (
+	Instruction LandType = iota
+	Forest
+	Mountain
+	Desert
+	Swamp
+	Plains
+)
+
 type TableTopRulerGame struct {
+	GameStarted                bool
 	Players                    []Player
 	LandDeck                   []LandTile
-	AvailalbeSpecialCharacters []SpecialCharacter
+	AvailableSpecialCharacters []SpecialCharacter
 	PlayingDeck                []any
+}
+
+type DomainSpot struct {
+	Land          LandTile
+	Upgrade       Improvement
+	Fortification Fortification
 }
 
 type Player struct {
 	Name           string
 	Code           string
 	PrestigePoints int
+	StandingArmy   []Unit
+	Domain         []DomainSpot
+	Gold           int
+	Hand           []any
 }
 
 type LandTile struct {
-	TileType string
+	TileType LandType
 	TileName string
+	Pict     *ebiten.Image
+}
+
+type Unit struct {
+	Name           string
+	Pict           *ebiten.Image
+	CombatValue    int
+	SpecialAbility SpecialAbility
+}
+
+type Improvement struct {
+	Name         string
+	Pict         *ebiten.Image
+	DefenseValue int
+	GoldValue    int
+}
+
+type Fortification struct {
+	Name                 string
+	Pict                 *ebiten.Image
+	DefensePrestigeValue int
 }
 
 type SpecialCharacter struct {
 	Name           string
 	Pict           *ebiten.Image
 	CombatValue    int
-	SpecialAbility string
+	SpecialAbility SpecialAbility
 }
--- main.go
+++ main.go
@@ -19,11 +19,31 @@ func main() {
 	//	Players: make([]*SharedData.Player, 0, 20), // no more than 20 concurrent players
 	//	Gold:    makeGold(),
 	//}
-	mainGame := "Hello World"
+	mainGame := TableTopRulerGame{
+		Players:                    make([]Player, 0, 4), // 2-4 concurrent players
+		LandDeck:                   make([]LandTile, 0, 54),
+		AvailableSpecialCharacters: make([]SpecialCharacter, 0, 11),
+		PlayingDeck:                make([]any, 0, 124),
+	}
 	RunServer(host, mainGame)
 
 }
 
-func RunServer(host enet.Host, game any) {
+func RunServer(host enet.Host, game TableTopRulerGame) {
+	for { //for ever
+		event := host.Service(1000) // Wait until the next event, 1000 is timeout
+		if event.GetType() == enet.EventNone {
+			continue // No events no event means don't do anything
+		}
+		switch event.GetType() {
+		case enet.EventConnect:
+			if game.GameStarted {
+				log.Printf("Client Tried to connect while game is already started: %s", event.GetPeer().GetAddress().String())
+				continue
+			} else {
+				log.Printf("Client connected: %s", event.GetPeer().GetAddress().String())
 
+			}
+		}
+	}
 }
