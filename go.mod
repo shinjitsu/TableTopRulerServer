d734b497905df082782df88b2ef63c31b5e004b3
shinjitsu <shinjitsu@auxilioabalto.me>
2024-10-11 16:01:30 -0400

    starting to move to grpc. Not yet working

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module TableTopRulerServer
 go 1.23.2
 
 require (
-	github.com/codecat/go-enet v0.0.0-20240719082706-af0e3f4fecfb
 	github.com/hajimehoshi/ebiten/v2 v2.8.1
+	google.golang.org/grpc v1.67.1
 )
 
 require (
@@ -12,6 +12,10 @@ require (
 	github.com/ebitengine/hideconsole v1.0.0 // indirect
 	github.com/ebitengine/purego v0.8.0 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
+	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,49 +1,45 @@
 package main
 
 import (
-	"github.com/codecat/go-enet"
+	"TableTopRulerServer/GameData"
+	"google.golang.org/grpc"
 	"log"
+	"net"
 )
 
+type Server struct {
+	GameData.UnimplementedTableTopRulerServiceServer
+}
+
 func main() {
-	// Initialize enet
-	enet.Initialize()
-
-	// Create a host listening on  port 8095
-	host, err := enet.NewHost(enet.NewListenAddress(8095), 32, 1, 0, 0)
+	listener, err := net.Listen("tcp", ":9090")
 	if err != nil {
-		log.Fatal("Couldn't create host: %s", err.Error())
-		return
+		panic(err)
 	}
-	//mainGame := SharedData.MPgame{
-	//	Players: make([]*SharedData.Player, 0, 20), // no more than 20 concurrent players
-	//	Gold:    makeGold(),
-	//}
-	mainGame := TableTopRulerGame{
-		Players:                    make([]Player, 0, 4), // 2-4 concurrent players
-		LandDeck:                   make([]LandTile, 0, 54),
-		AvailableSpecialCharacters: make([]SpecialCharacter, 0, 11),
-		PlayingDeck:                make([]any, 0, 124),
-	}
-	RunServer(host, mainGame)
-
-}
-
-func RunServer(host enet.Host, game TableTopRulerGame) {
-	for { //for ever
-		event := host.Service(1000) // Wait until the next event, 1000 is timeout
-		if event.GetType() == enet.EventNone {
-			continue // No events no event means don't do anything
-		}
-		switch event.GetType() {
-		case enet.EventConnect:
-			if game.GameStarted {
-				log.Printf("Client Tried to connect while game is already started: %s", event.GetPeer().GetAddress().String())
-				continue
-			} else {
-				log.Printf("Client connected: %s", event.GetPeer().GetAddress().String())
-
-			}
-		}
+	server := grpc.NewServer()
+	Game := Server{}
+	GameData.RegisterTableTopRulerServiceServer(server, &Game)
+	log.Println("Server listening at", listener.Addr())
+	if err := server.Serve(listener); err != nil {
+		log.Fatalln("Server exited with error:", err)
 	}
 }
+
+//func RunServer(host enet.Host, game TableTopRulerGame) {
+//	for { //for ever
+//		event := host.Service(1000) // Wait until the next event, 1000 is timeout
+//		if event.GetType() == enet.EventNone {
+//			continue // No events no event means don't do anything
+//		}
+//		switch event.GetType() {
+//		case enet.EventConnect:
+//			if game.GameStarted {
+//				log.Printf("Client Tried to connect while game is already started: %s", event.GetPeer().GetAddress().String())
+//				continue
+//			} else {
+//				log.Printf("Client connected: %s", event.GetPeer().GetAddress().String())
+//
+//			}
+//		}
+//	}
+//}
